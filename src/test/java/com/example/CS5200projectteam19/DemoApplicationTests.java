a3d83d249b2089088a0772007cc26d3a6ed78357
zefanzhang <zhang.zef@husky.neu.edu>
2020-03-29 18:28:41 -0400

    Initial project commit

---
+++ src/test/java/com/example/CS5200projectteam19/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CS5200projectteam19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
