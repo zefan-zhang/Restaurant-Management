60542222c00aa1380ae8d0fff6b011ba360dad1e
zefanzhang <zhang.zef@husky.neu.edu>
2020-04-04 03:05:36 -0400

    fixs models naming issues

--- src/main/java/edu/northeastern/cs5200/models/OrderList.java
+++ src/main/java/edu/northeastern/cs5200/models/OrderList.java
@@ -1,9 +1,12 @@
 package edu.northeastern.cs5200.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
@@ -11,38 +14,22 @@ import java.util.Collection;
 
 @Entity
 @Table(name = "orderList")
-public class OrderList {
+public class OrderDetail {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
   private int quantity;
 
-  private int orderId;
-
-  @OneToMany(mappedBy = "orderList")
-  private Collection<FoodItem> foodItems;
+  @ManyToOne
+  @JsonIgnore
+  private FoodItem foodItem;
 
   @OneToOne
   private Order order;
 
-  public OrderList() {
+  public OrderDetail() {
   }
 
-  public int getOrderId() {
-    return orderId;
-  }
-
-  public void setOrderId(int orderId) {
-    this.orderId = orderId;
-  }
-
-  public Collection<FoodItem> getFoodItems() {
-    return foodItems;
-  }
-
-  public void setFoodItems(Collection<FoodItem> foodItems) {
-    this.foodItems = foodItems;
-  }
 
   public int getId() {
     return id;
@@ -66,5 +53,13 @@ public class OrderList {
 
   public void setOrder(Order order) {
     this.order = order;
+  }
+
+  public FoodItem getFoodItem() {
+    return foodItem;
+  }
+
+  public void setFoodItem(FoodItem foodItem) {
+    this.foodItem = foodItem;
   }
 }
