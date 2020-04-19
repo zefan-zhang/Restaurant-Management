4d7a4f14c6edfb530b7144700c9f0a1da21669bd
zefanzhang <zhang.zef@husky.neu.edu>
2020-04-19 02:12:39 -0400

    add food category

--- src/main/java/edu/northeastern/cs5200/models/FoodCategory.java
+++ src/main/java/edu/northeastern/cs5200/models/FoodCategory.java
@@ -1,6 +1,6 @@
 package edu.northeastern.cs5200.models;
 
 public enum FoodCategory {
-  appetizer, mainDish, salad, meat, dessert, beverage
+  appetizer, mainDish, salad, meat, dessert, beverage, drink, juice, liquor
 
 }
